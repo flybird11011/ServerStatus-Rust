4b27451d62ac52bb682b6cc0978719c508c6e0b7
flybird11011 <63496013+flybird11011@users.noreply.github.com>
2023-12-24 21:22:00 +0800

    Update one-touch.sh

--- one-touch.sh
+++ one-touch.sh
@@ -9,20 +9,20 @@ cd ${WORKSPACE}
 OS_ARCH="x86_64"
 latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
+#wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
 wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
 
-unzip -o "server-${OS_ARCH}-unknown-linux-musl.zip"
+#unzip -o "server-${OS_ARCH}-unknown-linux-musl.zip"
 unzip -o "client-${OS_ARCH}-unknown-linux-musl.zip"
 
 # systemd service
-mv -v stat_server.service /etc/systemd/system/stat_server.service
+#mv -v stat_server.service /etc/systemd/system/stat_server.service
 mv -v stat_client.service /etc/systemd/system/stat_client.service
 
 systemctl daemon-reload
 
 # 启动
-systemctl start stat_server
+#systemctl start stat_server
 systemctl start stat_client
 
 # 状态查看
